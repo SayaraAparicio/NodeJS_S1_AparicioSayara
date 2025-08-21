12629afbb0b8b56972504881bfccc475d9d6bad4
SayaraAparicio <sayara.arciniegas@gmail.com>
2025-08-21 07:40:54 -0500

    feat: :rocket: workshop

--- Dia10/app.js
+++ Dia10/app.js
@@ -1,1 +1,31 @@
-console.log("Wenas")
+const prompt= require("prompt-sync")();
+
+const {UsuarioModel} = require('./models/userModel');
+const {UsuarioView} = require('./views/userView');
+const {UsuarioController} = require('./controllers/userController');
+
+const model = new UsuarioModel();
+const view = new UsuarioView();
+const controller = new UsuarioController(modelo, vista);
+
+async function main() {
+    let booleanito = false;
+    while (!booleanito) {
+        view.mostrarMenu();
+        const op = view.pedirOpcion(prompt);
+        switch (op) {
+            case "1":
+                await controller.registrarUsuario(prompt);
+                break;
+            case "2":
+                await controller.mostrarUsuarios();
+                break;
+            case "3":
+                booleanito = true;
+                break;
+            default:
+                console.log("Opción no válida");
+                break;
+        }
+    }
+}
